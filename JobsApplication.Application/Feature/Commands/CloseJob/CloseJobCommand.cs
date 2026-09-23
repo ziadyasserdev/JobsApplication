992112c969c30e6efb198c6faab6457ec742fda3
Ziad <ziadyasserabtelmiget@gmail.com>
2026-09-23 22:42:06 +0300

    Task

---
+++ JobsApplication.Application/Feature/Commands/CloseJob/CloseJobCommand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobsApplication.Application.Feature.Commands.CloseJob
+{
+    internal class CloseJobCommand
+    {
+    }
+}
